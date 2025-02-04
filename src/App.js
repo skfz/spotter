70278ea1773c1d0b2fdb6d69b0e6aec92442bed1
Sarfaraz <skfz.14@gmail.com>
2025-02-04 19:42:49 +0400

    First level filters

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Typography, Button, Autocomplete, Box, Select, MenuItem, FormGroup } from "@mui/material";
+import { useState } from "react";
+import PersonsModal from "./Components/PersonsModal";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [tripType, setTripType] = useState('Round Trip');
+    const [seating, setSeating] = useState('Economy');
+    const [persons, setPersons] = useState({
+        adults: 1,
+        children: 0,
+        infants_in_seat: 0,
+        infants_on_lap: 0
+    })
+
+    return (
+        <Container maxWidth="lg" sx={{ backgroundColor: "#fff", marginTop: "50px", textAlign: "center", padding: "20px" }}>
+            <Typography variant="h2" gutterBottom>App</Typography>
+            <Box sx={{ border: 1, borderColor: "text.disabled", borderRadius: 1, boxShadow: 2, padding: "20px" }}>
+                <FormGroup>
+                    <Box sx={{ display: 'flex', flexDirection: 'row', gap: 2 }}>
+                        <Select
+                            id="sp-trip-type"
+                            value={tripType}
+                            onChange={(e) => setTripType(e.target.value)}>
+                                <MenuItem value="Round Trip">Round Trip</MenuItem>
+                                <MenuItem value="One Way">One Way</MenuItem>
+                        </Select>
+                        <PersonsModal persons={persons} setPersons={setPersons} />
+                        <Select
+                            id="sp-seating"
+                            value={seating}
+                            onChange={(e) => setSeating(e.target.value)}>
+                                <MenuItem value="Economy">Economy</MenuItem>
+                                <MenuItem value="Premium Economy">Premium Economy</MenuItem>
+                                <MenuItem value="Business">Business</MenuItem>
+                                <MenuItem value="First">First</MenuItem>
+                        </Select>
+                    </Box>
+                </FormGroup>
+            </Box>
+        </Container>
+    )
 }
 
 export default App;
---
+++ src/Components/PersonsModal.js
@@ -0,0 +1,57 @@
+import { Box, Popover, Button, Typography } from "@mui/material";
+import { styled } from '@mui/material/styles';
+import React, { useState } from "react";
+
+export default function PersonsModal({ persons, setPersons }) {
+
+    const [anchorEl, setAnchorEl] = useState(false);
+    
+    const open = Boolean(anchorEl);
+    const id = open ? 'sp-popover' : undefined;
+
+    const CounterButton = styled(Button) (({ theme, disabled=false }) => ({
+        backgroundColor: disabled ? theme.palette.grey[200] : theme.palette.primary.light,
+        color: disabled ? theme.palette.grey[800] : theme.palette.info.dark,
+    }))
+
+    const BoxOuter = styled(Box) (() => ({
+        display: 'flex', flexDirection: 'row', gap: 20, alignItems: 'center'
+    }))
+
+    const BoxInner = styled(Box) (() => ({
+        display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center' 
+    }))
+
+    const updateCount = (type, count) => {
+        setPersons((prev) => ({
+            ...prev,
+            [type]: prev[type] + count
+        }));
+    }
+
+    return <>
+        <Button aria-describedby={id} variant="outlined" onClick={(e) => setAnchorEl(e.currentTarget)} >Persons</Button>
+        <Popover id={id} open={open} anchorEl={anchorEl} onClose={() => setAnchorEl(null)} anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+        }}>
+            <Box sx={{ display: 'flex', flexDirection:'column', padding: '10px', gap: 2 }}>
+                {Object.keys(persons).map((type, index) => {
+                    const min = type === 'adults' ? 1 : 0;
+                    let label = type.replace(/ /g,"_");
+                    label = label.charAt(0).toUpperCase() + label.slice(1);
+                    return <BoxOuter key={`${type}_${index}`}>
+                        <Box sx={{ width: '50%' }}>
+                            <Typography>{label}</Typography>
+                        </Box>
+                        <BoxInner>
+                            <CounterButton disabled={persons[type] == min} onClick={() => updateCount(type, -1)}>-</CounterButton>
+                            <Typography sx={{ padding: 1 }}>{persons[type]}</Typography>
+                            <CounterButton onClick={() => updateCount(type, 1)}>+</CounterButton>
+                        </BoxInner>
+                    </BoxOuter>
+                })}
+            </Box>
+        </Popover>
+    </>
+}
