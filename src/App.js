fc1206b0eaca5c9a8de7da5b23e2cf4753fed69e
Sarfaraz <skfz.14@gmail.com>
2025-02-05 20:11:17 +0400

    Implement search and list flights

--- src/App.js
+++ src/App.js
@@ -1,51 +1,121 @@
-import { Container, Typography, Button, Autocomplete, Box, Select, MenuItem, FormGroup } from "@mui/material";
+import { Container, Typography, Button, Autocomplete, Box, Select, MenuItem, FormGroup, Grid2 } from "@mui/material";
 import { useState } from "react";
 import PersonsModal from "./Components/PersonsModal";
 import AirportSearch from "./Components/AirportSearch";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs from "dayjs";
+import FlightList from "./Components/FlightList";
 
 function App() {
 
     const [tripType, setTripType] = useState('Round Trip');
-    const [seating, setSeating] = useState('Economy');
+    const [seating, setSeating] = useState('economy');
     const [persons, setPersons] = useState({
         adults: 1,
         children: 0,
-        infants_in_seat: 0,
-        infants_on_lap: 0
+        infants: 0,
     });
     const [origin, setOrigin] = useState(null);
     const [destination, setDestination] = useState(null);
+    const [fromDate, setFromDate] = useState(dayjs());
+    const [toDate, setToDate] = useState(dayjs());
+    const [flightsData, setFlightsData] = useState([]);
+
+    const handleFlightSearch = async() => {
+        const from_date = dayjs(fromDate).format('YYYY-MM-DD');
+        const to_date = dayjs(toDate).format('YYYY-MM-DD');
+        const url = `https://sky-scrapper.p.rapidapi.com/api/v2/flights/searchFlights?originSkyId=${origin.skyId}&destinationSkyId=${destination.skyId}
+        &originEntityId=${origin.entityId}&destinationEntityId=${destination.entityId}&date=${from_date}&returnDate=${to_date}&cabinClass=${seating}&adults=${persons.adults}
+        &childrens=${persons.children}&infants=${persons.infants}&sortBy=best&currency=USD&market=en-US&countryCode=US`;
+        const options = {
+            method: 'GET',
+            headers: {
+                'x-rapidapi-key': '4467bc502fmsh10af65d6033c2f2p10c22ejsnba125264a1fb',
+                'x-rapidapi-host': 'sky-scrapper.p.rapidapi.com'
+            }
+        };
+
+        try {
+            const response = await fetch(url, options);
+            const result = await response.json();
+            if (result?.data) {
+                setFlightsData(result.data);
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
     return (
-        <Container maxWidth="lg" sx={{ backgroundColor: "#fff", marginTop: "50px", textAlign: "center", padding: "20px" }}>
-            <Typography variant="h2" gutterBottom>App</Typography>
+        <Container sx={{ backgroundColor: "#fff", marginTop: "50px", textAlign: "center" }}>
+            <Typography variant="h2" gutterBottom>Flights</Typography>
             <Box sx={{ border: 1, borderColor: "text.disabled", borderRadius: 1, boxShadow: 2, padding: "20px" }}>
-                <FormGroup>
-                    <Box sx={{ display: 'flex', flexDirection: 'row', gap: 2 }}>
+                <Grid2 container spacing={4}>
+                    <Grid2 size={{md: 2, xs: 12}}>
                         <Select
+                            sx={{ width: '100%' }}
                             id="sp-trip-type"
                             value={tripType}
                             onChange={(e) => setTripType(e.target.value)}>
                                 <MenuItem value="Round Trip">Round Trip</MenuItem>
                                 <MenuItem value="One Way">One Way</MenuItem>
                         </Select>
+                    </Grid2>
+                    <Grid2 size={{md: 2, xs: 12}}>
                         <PersonsModal persons={persons} setPersons={setPersons} />
+                    </Grid2>
+                    <Grid2 size={{md: 2, xs: 12}}>
                         <Select
+                            sx={{ width: '100%' }}
                             id="sp-seating"
                             value={seating}
                             onChange={(e) => setSeating(e.target.value)}>
-                                <MenuItem value="Economy">Economy</MenuItem>
-                                <MenuItem value="Premium Economy">Premium Economy</MenuItem>
-                                <MenuItem value="Business">Business</MenuItem>
-                                <MenuItem value="First">First</MenuItem>
+                                <MenuItem value="economy">Economy</MenuItem>
+                                <MenuItem value="premium_economy">Premium Economy</MenuItem>
+                                <MenuItem value="business">Business</MenuItem>
+                                <MenuItem value="first">First</MenuItem>
                         </Select>
-                    </Box>
-                    <Box sx={{ display: 'flex', flexDirection: 'row', gap: 8, marginTop: 2}}>
+                    </Grid2>
+                </Grid2>
+                <Grid2 container spacing={4} sx={{ marginTop: 8}}>
+                    <Grid2 size={{md:3, xs: 12}}>
                         <AirportSearch location={origin} setLocation={setOrigin} label="Origin" />
+                    </Grid2>
+                    <Grid2 size={{md:3, xs: 12}}>
                         <AirportSearch location={destination} setLocation={setDestination} label="Destination" />
-                    </Box>
-                </FormGroup>
+                    </Grid2>
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <Grid2 size={{md:3, xs: 12}}>
+                            <DatePicker minDate={dayjs()} label="Depart Date" value={fromDate} onChange={(newDate) => {
+                                setFromDate(newDate);
+                                if (newDate.isAfter(dayjs(toDate))) {
+                                    setToDate(newDate)
+                                }
+                            }} 
+                                slotProps={{
+                                    textField: {
+                                    helperText: 'MM-DD-YYYY',
+                                    },
+                                }} />
+                        </Grid2>
+                        <Grid2 size={{md:3, xs: 12}}>
+                            <DatePicker minDate={fromDate} label="Return Date" value={toDate} onChange={(newDate) => setToDate(newDate)} 
+                                slotProps={{
+                                    textField: {
+                                    helperText: 'MM-DD-YYYY',
+                                    },
+                                }} />
+                        </Grid2>
+                    </LocalizationProvider>
+                    
+                </Grid2>
+                <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop: '20px' }}>
+                    <Button variant="contained" disabled={origin === null || destination == null} size="large" onClick={handleFlightSearch}>Submit</Button>
+                </Box>
+                
             </Box>
+            <FlightList flightsData={flightsData} />
         </Container>
     )
 }
---
+++ src/Components/FlightList.js
@@ -0,0 +1,76 @@
+import { Avatar, Box, Grid2, TableContainer, TableBody, TableRow, Typography, Paper, TableHead, TableCell, Table } from "@mui/material";
+import dayjs from "dayjs";
+import advancedFormat from "dayjs/plugin/advancedFormat";
+import duration from "dayjs/plugin/duration";
+import utc from "dayjs/plugin/utc";
+
+export default function FlightList({ flightsData }) {
+    
+    dayjs.extend(advancedFormat);
+    dayjs.extend(utc);
+    dayjs.extend(duration)
+    
+    const flights = flightsData?.itineraries;
+
+    const formatDate = (dateString) => {
+        return dayjs.utc(dateString).format('ddd, MMM DD h:mm A');
+    }
+
+    function formatDuration(minutes) {
+        const time = dayjs.duration(minutes, "minutes");
+        return `${time.hours()}h ${time.minutes()}m`;
+    }
+
+    return (
+            <>
+            {flights && flights.length > 0 && flights.map((flight, index) => {
+
+                return <TableContainer component={Paper} sx={{ marginBottom: 4, marginTop: 4 }} key={index}>
+                <Table>
+                    <TableBody sx={{ width: '100%' }}>
+                        <TableRow>
+                            <TableCell colSpan={2}>
+                                <Typography fontWeight="bold">{flight.legs[0].origin.city} to {flight.legs[0].destination.city}</Typography>
+                            </TableCell>
+                            <TableCell  align="center">
+                                <Typography fontWeight="bold">{flight.price.formatted}</Typography>
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell align="center">
+                                <Avatar sx={{ margin: '0 auto' }} variant="square" alt="carrier_logo" src={flight.legs[0].carriers.marketing[0].logoUrl} loading="lazy" />
+                                <Typography>{flight.legs[0].carriers.marketing[0].name}</Typography>
+                            </TableCell>
+                            <TableCell align="center">
+                                <Typography>{formatDate(flight.legs[0].departure)} - {formatDate(flight.legs[0].arrival)}</Typography>
+                                <Typography>{formatDuration(flight.legs[0].durationInMinutes)}</Typography>
+                            </TableCell>
+                            <TableCell align="center">
+                                <Typography>{flight.legs[0].stopCount === 0 ? 'Nonstop' : `${flight.legs[0].stopCount} stop`}</Typography>
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell colSpan={3}>
+                                <Typography fontWeight="bold">{flight.legs[1].origin.city} to {flight.legs[1].destination.city}</Typography>
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell align="center">
+                                <Avatar sx={{ margin: '0 auto' }} variant="square" alt="carrier_logo" src={flight.legs[1].carriers.marketing[0].logoUrl} loading="lazy" />
+                                <Typography>{flight.legs[1].carriers.marketing[0].name}</Typography>
+                            </TableCell>
+                            <TableCell align="center">
+                                <Typography>{formatDate(flight.legs[1].departure)} - {formatDate(flight.legs[1].arrival)}</Typography>
+                                <Typography>{formatDuration(flight.legs[1].durationInMinutes)}</Typography>
+                            </TableCell>
+                            <TableCell align="center">
+                                <Typography>{flight.legs[1].stopCount === 0 ? 'Nonstop' : `${flight.legs[1].stopCount} stop`}</Typography>
+                            </TableCell>
+                    </TableRow>
+                    </TableBody>
+                </Table>
+                </TableContainer>
+            })}
+        </>
+    )
+}
